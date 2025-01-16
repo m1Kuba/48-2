40237bad715c30d6bd79daf99ab2934979293252
m1Kuba <kubamelisbekov002@gmail.com>
2025-01-17 01:44:07 +0600

    hw1

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+import random
+
+
+def main_view(request):
+    return HttpResponse(f"Hello World {random.randint(1, 100)}")
+
+
+def html_view(request):
+    return render(request, "main.html")
+
+# Create your views here.
